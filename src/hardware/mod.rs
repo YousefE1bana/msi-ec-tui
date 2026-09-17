da33f829dcba4bb01c1a7584b29d6d9848b9f25e
Yousef Osama <y3usef.osama@gmail.com>
2026-09-18 01:18:28 +0300

    feat: add testable system path abstraction

--- src/hardware/mod.rs
+++ src/hardware/mod.rs
@@ -3,11 +3,13 @@
 pub mod backend;
 pub mod capabilities;
 pub mod device;
+pub mod paths;
 pub mod values;
 
 pub use backend::{BackendError, EcBackend};
 pub use capabilities::{BacklightCapability, Capabilities};
 pub use device::{DeviceInfo, HardwareSnapshot};
+pub use paths::{PathValidationError, SystemPaths};
 pub use values::{
     FanMode, FanPercent, ModeValidationError, SensorValueError, ShiftMode, TemperatureCelsius,
 };
